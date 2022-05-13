491f1b6b5fc4dfdd12b2fc858dd3bb5cfc5c75a8
thisara20 <chandrawanshathisara@gmail.com>
2022-05-13 21:10:26 +0530

    Did changes to pay.java, pays.js and pays.jsp

---
+++ PayFrontend/src/main/java/com/PayService.java
@@ -0,0 +1,79 @@
+package com;
+
+import model.Pay;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/Pays")
+
+public class PayService {
+
+	Pay payObj = new Pay();
+     
+	//user read payments 
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML) //respond media type
+	
+	public String readPay() {
+		
+		
+		return payObj.readPay();
+	}
+	
+	//user insert a payment
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)  //request media type
+	@Produces(MediaType.TEXT_PLAIN) //respond media type
+	
+	 
+	public String insertpay(
+	 @FormParam("payDate") String payDate,
+	 @FormParam("name") String name,
+	 @FormParam("email") String email,
+	 @FormParam("amount") String amount,
+	 @FormParam("accNo") String accNo,
+	 @FormParam("ccv") String ccv,
+	 @FormParam("expireDate") String expireDate)
+	{
+	 String output = payObj.insertPay( payDate,  name,  email, amount,  accNo,  ccv, expireDate);
+	return output;
+	}
+	
+	//user update the payment using payment ID
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)  //request media type
+	@Produces(MediaType.TEXT_PLAIN)  //respond media type
+	public String updatePay(String PayData)
+	{
+	//Convert the input string to a JSON object
+	 JsonObject payObject = new JsonParser().parse(PayData).getAsJsonObject();
+	
+	 //Read the values from the JSON object and capture the json data set
+	 String payID = payObject.get("payID").getAsString();
+	 String payDate = payObject.get("payDate").getAsString();
+	 String name = payObject.get("name").getAsString();
+	 String email= payObject.get("email").getAsString();
+	 String amount = payObject.get("amount").getAsString();
+	 String accNo = payObject.get("accNo").getAsString();
+	 String ccv = payObject.get("ccv").getAsString();
+	 String expireDate = payObject.get("expireDate").getAsString();
+	  
+	 String output = payObj.updatePay(payID,payDate,name,email,amount, accNo,ccv, expireDate );
+	return output;
+	}
+	
+	 
+
+}
--- PayFrontend/src/main/webapp/pays.jsp
+++ PayFrontend/src/main/webapp/pays.jsp
@@ -1,20 +1,26 @@
+<%@page import="model.Pay"%>
+
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
+ 
+  
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Payment Management</title>
 <link rel="stylesheet" href="Views/bootstrap.min.css">
 <script src="Components/jquery-3.2.1.min.js"></script>
 <script src="Components/pays.js"></script>
 
 </head>
 <body>
+<div class="container"><div class="row"><div class="col-6">
+<h1>Payment Management V10.1</h1>
 
 	<form id="formPay" name="formPay" method="post" action="pays.jsp">
 
-		Payment Date <input id="paydate" name="paydate" type="text"
+		Payment Date <input id="payDate" name="payDate" type="text"
 			class="form-control form-control-sm"> <br> Name: <input
 			id="name" name="name" type="text"
 			class="form-control form-control-sm"> <br> Email: <input
@@ -24,14 +30,22 @@
 			class="form-control form-control-sm"> <br>Account No: <input
 			id="accNo" name="accNo" type="text"
 			class="form-control form-control-sm"> <br>CCV: <input
-			id="ccv" name="ccv" type="text"
-			class="form-control form-control-sm"> <br>Card Expire Date:
-		  <input id="expireDate" name="expireDate" type="text"
-			class="form-control form-control-sm"> <br> <input
+			id="ccv" name="ccv" type="text" class="form-control form-control-sm">
+		<br>Card Expire Date: <input id="expireDate" name="expireDate"
+			type="text" class="form-control form-control-sm"> <br> <input
 			id="btnSave" name="btnSave" type="button" value="Save"
 			class="btn btn-primary"> <input type="hidden"
 			id="hidPayIDSave" name="hidPayIDSave" value="">
 	</form>
-
+<div id="alertSuccess" class="alert alert-success"></div>
+<div id="alertError" class="alert alert-danger"></div>
+<br>
+<div id="divItemsGrid">
+ <%
+ Pay payObj = new Pay();
+ out.print(payObj.readPay());
+ %>
+</div>
+</div> </div> </div>
 </body>
 </html>
